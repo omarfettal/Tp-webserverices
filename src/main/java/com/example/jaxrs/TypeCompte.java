279aebe50fac868357ee60205a583552915c21e2
Omar <omarfettal37@gmail.com>
2024-10-31 23:10:23 +0100

    first-commit

---
+++ src/main/java/com/example/jaxrs/TypeCompte.java
@@ -0,0 +1,5 @@
+package com.example.jaxrs;
+
+public enum TypeCompte {
+    COURANT, EPARGNE
+}
---
+++ src/test/java/com/example/jaxrs/JaxRsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaxrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaxRsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
